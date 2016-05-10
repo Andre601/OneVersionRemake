70497a6d2262172cfdc3916f59fd7f624e782a53
Johnnywoof <info@johnnywoof.net>
2016-05-10 15:04:50 -0400

    Release 3.1 - Updated to support latest bungeecord build. Also is backwards compatible.

--- src/me/johnnywoof/OneVersion.java
+++ src/me/johnnywoof/OneVersion.java
@@ -12,9 +12,12 @@ import net.md_5.bungee.config.ConfigurationProvider;
 import net.md_5.bungee.config.YamlConfiguration;
 import net.md_5.bungee.event.EventHandler;
 import net.md_5.bungee.event.EventPriority;
-import net.md_5.bungee.protocol.Protocol;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class OneVersion extends Plugin implements Listener {
 
@@ -32,7 +35,7 @@ public class OneVersion extends Plugin implements Listener {
             Configuration yml = ConfigurationProvider.getProvider(YamlConfiguration.class).load(this.getConfig());
 
             this.connect_message = ChatColor.translateAlternateColorCodes('&', yml.getString("kick-message",
-					"This server has been updated to 1.9!.newline.Please use the latest minecraft version")
+					"This server has been updated!.newline.Please use the latest minecraft version")
 					.replaceAll(".newline.", "\n"));
 
 			this.protocol_name = ChatColor.stripColor(yml.getString("protocol-name", "null"));
@@ -43,11 +46,8 @@ public class OneVersion extends Plugin implements Listener {
 			this.protocol_id = yml.getInt("protocol-version", -1);
 
 			if (this.protocol_id < 0) {//-1 value for example
-
-				for (int version : Protocol.supportedVersions)
-					if (version > this.protocol_id)
-						this.protocol_id = version;
-
+				//noinspection deprecation
+				this.protocol_id = this.getProxy().getProtocolVersion();
 			}
 
         } catch (IOException e) {
